263407dbb0da9b8fad7984d5bb71c681ec86d3cd
Debak Sen <52042726+debaksen@users.noreply.github.com>
2020-11-08 22:32:13 +0530

    Update 98. Validate Binary Search Tree.java

--- solutions/98. Validate Binary Search Tree.java
+++ solutions/98. Validate Binary Search Tree.java
@@ -1,31 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        
-        long min = Long.MIN_VALUE;
-        long max = Long.MAX_VALUE;
-        
-        // for left sub tree we change the uppervalue - as the curr node's val
-        
-        // for right sub tree change the lower value - as the curr node's val
-        
-        if(root == null) return true;
-        
-        return (helperFunc(root, min, max));
-    }
-               
-    private boolean helperFunc(TreeNode root , long min, long  max){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        return (p.val == q.val && helperSame(p.left,q.left) && helperSame(p.right,q.right));
+    }
+    
+    private static boolean helperSame(TreeNode p, TreeNode q){
+        
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        
+        if(p.val != q.val) return false;
+        
+        return (helperSame(p.left,q.left) && helperSame(p.right,q.right));
+    }
+}
