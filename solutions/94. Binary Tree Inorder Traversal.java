6ef9efef33f3b7815c45ee6ea8055708b096a10b
Debak Sen <52042726+debaksen@users.noreply.github.com>
2020-11-08 22:31:04 +0530

    0 ms

--- solutions/94. Binary Tree Inorder Traversal.java
+++ solutions/94. Binary Tree Inorder Traversal.java
@@ -16,16 +16,18 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
-        List<Integer> list = new ArrayList<Integer>();
-        
-        Stack<TreeNode> st = new Stack<>();
-     
-        
-        while(root != null || !st.isEmpty()){
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        fillInOrder(root,list);
+        return list;
+    }
+    
+    private static void fillInOrder(TreeNode root , List<Integer> list){
+        if(root != null){
             
-            while(root != null){
-                st.push(root);
-                root = root.left;
-            }
-            
-            root = st.pop();
+            fillInOrder(root.left,list);
+            list.add(root.val);
+            fillInOrder(root.right,list);
+        }
+    }
+}
