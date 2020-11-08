7f1b9c7deb34625a9e8d4c53e0bb2954236a6e8d
Debak Sen <52042726+debaksen@users.noreply.github.com>
2020-11-08 22:37:25 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- solutions/144. Binary Tree Preorder Traversal.java
+++ solutions/144. Binary Tree Preorder Traversal.java
@@ -1,37 +1,65 @@
-        while(!st.isEmpty()){
-           
-            root = st.pop();
-            list.add(root.val);
-            
-            if(root.right != null){
-                st.push(root.right);
-            }
-            
-            if(root.left != null){
-                st.push(root.left);
-            }
-        }
-        
-        return list; 
-    }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Iterative approach  
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null) return list;
+        
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        
+        while(!st.isEmpty()){
+           
+            root = st.pop();
+            list.add(root.val);
+            
+            if(root.right != null){
+                st.push(root.right);
+            }
+            
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        
+        return list; 
+    }
 }
-​
+
 // Recursive approach
-​
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+   
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        fillPreOrder(root,list);
+        return list;       
+    }
+    
+    private static void fillPreOrder(TreeNode root, List<Integer> list){
+        
+        if(root != null){
+            list.add(root.val);
+            fillPreOrder(root.left,list);
+            fillPreOrder(root.right,list);
+        }
+    }
+}
